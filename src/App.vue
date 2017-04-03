52bdfbe5e36d1924dc053cea2154e89f8baa124e
Albert Pedrola <b.albemar@gmail.com>
2017-04-04 00:55:12 +0200

    paths

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://apedrolam.github.io/dist/logo.png')
     h1 Vue Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
