1a5f16c10bb3d401356acf251b5305039741d469
Albert Pedrola <b.albemar@gmail.com>
2017-04-05 22:51:39 +0200

    make images clickable

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 li
-    h2: a(:href="artist.url" target="_blank") {{ artist.name }}
-    img(:src="artist.image[2]['#text']")
+    h2: a(:href="artist.url" target="_blank")
+        p {{ artist.name }}
+        img(:src="artist.image[2]['#text']")
 </template>
 
 <script>
